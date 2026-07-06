ce172b3ec5f195a7f9bdff5932f8ed51a0f585dc
Pavel Ivanov <xeolit@gmail.com>
2026-07-06 15:00:28 +0300

    second task done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,54 @@
 package hw02unpackstring
 
 import (
-	"errors"
+    "errors"
+    "strings"
+    "unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+    type pair struct {
+        r     rune
+        count int
+    }
+    var pairs []pair
+
+    var lastRune rune
+    var lastCount int
+    var hasLast bool
+    lastWasDigit := false
+
+    for _, r := range s {
+        if unicode.IsDigit(r) {
+            if !hasLast {
+                return "", ErrInvalidString
+            }
+            if lastWasDigit {
+                return "", ErrInvalidString
+            }
+            lastCount = int(r - '0')
+            lastWasDigit = true
+        } else {
+            if hasLast {
+                pairs = append(pairs, pair{lastRune, lastCount})
+            }
+            lastRune = r
+            lastCount = 1
+            hasLast = true
+            lastWasDigit = false
+        }
+    }
+    if hasLast {
+        pairs = append(pairs, pair{lastRune, lastCount})
+    }
+
+    var sb strings.Builder
+    for _, p := range pairs {
+        if p.count > 0 {
+            sb.WriteString(strings.Repeat(string(p.r), p.count))
+        }
+    }
+    return sb.String(), nil
 }
